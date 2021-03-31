4d0d78cf0a78b6177349daee7bda9b1127f8b7c4
aoiyo <orjck54836@gmail.com>
2021-04-01 00:17:36 +0800

    ok

--- main.js
+++ main.js
@@ -42,6 +42,10 @@ var all = [
     },
 ]
 var i = '' || 0
+window.onload = open
+function open(){
+    $('#content2').css('display','none')
+}
 
 $('.aboutme').click(function(){
     $('#content2').fadeOut(100)
@@ -52,11 +56,12 @@ $('.portfolio').click(function(){
     $('#content1').fadeOut(100)
     $('#content2').show(kk = function(){  
         $('#content2').html(
-            '<a href="'+all[i].url+'" style="font-size:25px;color:#3c4f94;text-decoration: none">'+all[i].title+'</a><br><img src="'+all[i].img+'" width="600" height="300">'
+            '<a href="'+all[i].url+'" style="font-size:25px;color:#c31c1c;text-decoration: none">'+all[i].title+'</a><br><img src="'+all[i].img+'" width="80%" height="60%">'
         )
         $('#content2').append('<div style="display:inline-block;margin-top:2vw;cursor: pointer"><i class="fas fa-arrow-circle-left" style="width:80px;font-size:50px"></i><i class="fas fa-arrow-circle-right" style="width:80px;font-size:50px"></i></div>')
         
         $('.fa-arrow-circle-left').click(function(){
+            $('.fa-arrow-circle-left').animate({right:'200px'},500)
             if(i>0){
                 i--   
             }else if(i==0){
